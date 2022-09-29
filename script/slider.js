3e3b5b56fe9058b5d27997dc6599f76bef880ffd
antoebtfr <antoebtf@gmail.com>
2022-09-29 20:31:42 +0200

    edit

--- script/slider.js
+++ script/slider.js
@@ -1,32 +1,32 @@
 export default class Slider {
 
-    constructor(){
+    constructor() {
         this.activeSlide = 0;
         this.slideNumber = 0;
     }
 
-     async carouselSlideStart() {
+    async carouselSlideStart() {
         const slides = $('.slide');
         this.slideNumber = slides.length;
         slides.hide();
         slides.first().show();
         this.activeSlide = 1;
         $('#top-right-section').addClass('active-slide-' + this.activeSlide);
-    
+
         $('#btn-angle-previous').click(this.previousSlide);
         $('#btn-angle-next').click(this.nextSlide);
 
         $('#btn-angle-previous-mobile').click(this.previousSlide);
         $('#btn-angle-next-mobile').click(this.nextSlide);
-     }
- 
+    }
+
     previousSlide = () => {
-        this.activeSlide --;
+        this.activeSlide--;
         this.changeSlide();
     }
-    
+
     nextSlide = () => {
-        this.activeSlide ++;
+        this.activeSlide++;
         this.changeSlide();
     }
 
@@ -39,20 +39,20 @@ export default class Slider {
         this.changeBackgroundImage();
     }
 
-    slideLimitTester(){
+    slideLimitTester() {
         this.activeSlide <= 0 ? this.activeSlide = this.slideNumber : '';
         this.activeSlide > this.slideNumber ? this.activeSlide = 1 : '';
     }
 
-    changeBackgroundImage(){
+    changeBackgroundImage() {
         const imageBank = [
-            {id: 1, image: 'desktop-image-hero-1', ext: 'jpg'},
-            {id: 2, image: 'desktop-image-hero-2', ext: 'jpg'},
-            {id: 3, image: 'desktop-image-hero-3', ext: 'jpg'}
-        ] 
+            { id: 1, image: 'desktop-image-hero-1', ext: 'jpg' },
+            { id: 2, image: 'desktop-image-hero-2', ext: 'jpg' },
+            { id: 3, image: 'desktop-image-hero-3', ext: 'jpg' }
+        ]
 
-        const background = imageBank.filter( x => x.id == this.activeSlide)[0];
+        const background = imageBank.filter(x => x.id == this.activeSlide)[0];
 
-        $('#top-left-section').css('background-image', 'url("/images/' + background.image + '.'+ background.ext +'")');
+        $('#top-left-section').css('background-image', 'url("./images/' + background.image + '.' + background.ext + '")');
     }
 }
